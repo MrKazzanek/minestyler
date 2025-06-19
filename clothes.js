91a73205fc61c8f2c41cc72474c4d78b2d91ff86
MrKazzanek <161287375+MrKazzanek@users.noreply.github.com>
2025-06-19 21:10:34 +0200

    Update clothes.js

--- clothes.js
+++ clothes.js
@@ -3,17 +3,6 @@
 const clothesData = [
   {
     id: 2,
-    name: "MCC Cape Armlets (Steve)",
-    description: "These armlets pads match the MCC Cape from the MCC x Minecraft 15th Anniversary Party event.",
-    author: "MrKazanek",
-    authorLink: "https://bio.link/mrkazanek",
-    category: "Armlets",
-    imageUrl: "https://i.ibb.co/j902wCcD/Mine-Styler-images.png",
-    videoUrl: "movie/anim1.mp4",
-    textureUrl: "https://i.ibb.co/hxBGzrvK/MCC-Cape-Armlets-Steve.png",
-  },
-  {
-    id: 1,
     name: "MCC Cape Armlets (Alex)",
     description: "These armlets pads match the MCC Cape from the MCC x Minecraft 15th Anniversary Party event.",
     author: "MrKazanek",
@@ -23,5 +12,17 @@ const clothesData = [
     videoUrl: "movie/anim2.mp4",
     textureUrl: "https://i.ibb.co/zYt4N1n/MCC-Armlets-Alex.png",
   },
+  {
+    id: 1,
+    name: "MCC Cape Armlets (Steve)",
+    description: "These armlets pads match the MCC Cape from the MCC x Minecraft 15th Anniversary Party event.",
+    author: "MrKazanek",
+    authorLink: "https://bio.link/mrkazanek",
+    category: "Armlets",
+    imageUrl: "https://i.ibb.co/j902wCcD/Mine-Styler-images.png",
+    videoUrl: "movie/anim1.mp4",
+    textureUrl: "https://i.ibb.co/hxBGzrvK/MCC-Cape-Armlets-Steve.png",
+  },
+  
   // Dodaj więcej przedmiotów w tym formacie
 ];
