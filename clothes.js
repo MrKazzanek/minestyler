881ecd59758b316e41308b91430cd6cea6ee7d56
MrKazzanek <161287375+MrKazzanek@users.noreply.github.com>
2025-06-19 20:41:12 +0200

    Update clothes.js

--- clothes.js
+++ clothes.js
@@ -1,27 +1,17 @@
 // clothes.js (WERSJA ZE ŚCIEŻKAMI - WYMAGA LIVE SERVER)
 
 const clothesData = [
+  
   {
     id: 1,
-    name: "Złota Korona",
-    description: "Królewska korona dla prawdziwego władcy.",
-    author: "PixelMaster",
-    authorLink: "https://twitter.com/minecraft",
-    category: "Nakrycia głowy",
-    imageUrl: "assets/crown_preview.png",
-    videoUrl: "assets/crown_video.mp4",
-    textureUrl: "assets/crown_texture.png",
-  },
-  {
-    id: 2,
-    name: "Złote Naramienniki",
-    description: "Błyszczące naramienniki prosto z królewskiej zbrojowni.",
-    author: "ArtisanAlex",
-    authorLink: "https://www.minecraft.net/",
-    category: "Naramienniki",
-    imageUrl: "assets/pauldrons_preview.png",
-    videoUrl: "assets/pauldrons_video.mp4",
-    textureUrl: "assets/pauldrons_texture.png",
+    name: "MCC Cape Armlets (Steve)",
+    description: "These armlets pads match the MCC Cape from the MCC x Minecraft 15th Anniversary Party event.",
+    author: "MrKazanek",
+    authorLink: "https://bio.link/mrkazanek",
+    category: "Armlets",
+    imageUrl: "https://i.ibb.co/j902wCcD/Mine-Styler-images.png",
+    videoUrl: "movie/anim1.mp4",
+    textureUrl: "https://i.ibb.co/hxBGzrvK/MCC-Cape-Armlets-Steve.png",
   },
   // Dodaj więcej przedmiotów w tym formacie
 ];
