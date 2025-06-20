feab30b15976ba902cfecd1cf571295eb59705dd
MrKazzanek <161287375+MrKazzanek@users.noreply.github.com>
2025-06-20 13:11:15 +0200

    Update clothes.js

--- clothes.js
+++ clothes.js
@@ -2,6 +2,17 @@
 
 const clothesData = [
   {
+    id: 3,
+    name: "Mojang Office Cape Armlets (Steve)",
+    description: "These Armlets are a perfect match for the Mojang Office Cape from the Eerie Mojang office party event.",
+    author: "MrKazanek",
+    authorLink: "https://bio.link/mrkazanek",
+    category: "Armlets",
+    imageUrl: "https://i.ibb.co/pHQ0TsB/Mojang-Office-Cape-Armlets-Steve-Skin.png",
+    videoUrl: "movie/anim3.mp4",
+    textureUrl: "https://i.ibb.co/gLG3Mg57/Mojang-Office-Cape-Armlets-Steve.png",
+  },
+  {
     id: 2,
     name: "MCC Cape Armlets (Alex)",
     description: "These armlets match the MCC Cape from the MCC x Minecraft 15th Anniversary Party event.",
